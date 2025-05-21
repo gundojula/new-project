b4f61e010f744f0aee4157a98496dee7b2499e3d
chandrashekar <gundojula@gmaill.com>
2025-05-21 00:03:56 -0400

    added new shell script file

---
+++ chandra.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+URL="https://example.com/sample-data.csv"
+OUTPUT_DIR="/tmp/downloads"
+
+mkdir -p "$OUTPUT_DIR"
+
+wget -q -O "$OUTPUT_DIR/sample-data.csv" "$URL"
+
+if [ $? -eq 0 ]; then
+  echo "Download successful!"
+else
+  echo "Download failed!"
+fi
+
